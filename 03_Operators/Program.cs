f79c25e100558829540c2100708b15dead1d098a
cstazinski29 <cstazinski@gmail.com>
2022-02-02 12:06:24 -0500

    add 03_operators to github (note, missed some of the commands that were shown in zoom)

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿int additionResult = 5 + 2;
+Console.WriteLine("5 + 2 = " + additionResult);
+
+// int numOne = 5;
+
+int subtractionResult = 5 - 2;
+
+int multiplicationResult = 5 * 2;
+Console.WriteLine("5 * 2 = " + multiplicationResult);
+
+Console.WriteLine(10 % 2);
