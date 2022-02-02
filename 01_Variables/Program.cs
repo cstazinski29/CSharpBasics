9d3164b385a8d6ba5f3ff2f2a5f5c7c5952505e5
cstazinski29 <cstazinski@gmail.com>
2022-02-02 10:48:33 -0500

    adding 01_variables and 02_Types to the git project

---
+++ 01_Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿// string firstName = "Chris";
+// Console.WriteLine(firstName);
+
+Console.WriteLine("Wuts yo name?");
+string firstName = Console.ReadLine();
+Console.WriteLine("You said your name is " + firstName);
+
+Console.WriteLine("Wuts yo fav color?");
+
+// Console.ReadLine is taking information in form the user, so it is blank
+string color = Console.ReadLine();
+
+Console.WriteLine("You said your favorite color is " + color +", " + firstName);
