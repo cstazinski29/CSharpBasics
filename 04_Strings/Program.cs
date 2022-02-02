eadb990f80b512160d3771bdc3192d01e8521446
cstazinski29 <cstazinski@gmail.com>
2022-02-02 12:25:12 -0500

    added Strings to the github

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿string first = "The cars we sell are";
+string second = "BMW, Lexus, Mercedes and Tesla.";
+
+string concatenatedResult = first + " " + second;
+
+Console.WriteLine(concatenatedResult);
+
+string firstName = "Jenn";
+string lastName = "Williams";
+
+string compositeResult = string.Format("Her name is {0} {1}", firstName, lastName);
+Console.WriteLine(compositeResult);
+
+// string compositeResult = string.Format("Her name is {0} {1} {2} {3}", firstName, lastName, first, second);
+// Console.WriteLine(compositeResult);
+
+string interpolationResult = $"Her name is still {firstName} {lastName}!";
+Console.WriteLine(interpolationResult);
+Console.WriteLine($"Her name is still {firstName} {lastName}!");
