eb982fee096555dbb0e0f937fa991b8075d85bec
cstazinski29 <cstazinski@gmail.com>
2022-02-03 11:20:32 -0500

    adding switches to the solution / project

---
+++ 06_Conditionals/Switches/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Give name.");
+string name = Console.ReadLine();
+switch(name)
+{
+    case "Chris":
+    Console.WriteLine("sup");
+    break;
+    default:
+    Console.WriteLine("nope");
+    break;
+}
+
+Console.WriteLine("how feeling?");
+string feelingRating = Console.ReadLine();
+
+switch(feelingRating)
+{
+    case "5":
+    Console.WriteLine("cool");
+    break;
+    case "4":
+    Console.WriteLine("less cool");
+    break;
+    case "3":
+    Console.WriteLine("meh");
+    break;
+    default:
+    Console.WriteLine("ugh");
+    break;
+}
+
+// Console.WriteLine("how feeling number?");
+// int feelingNumber = Console.ReadLine();
+
+// switch(feelingNumber)
+// {
+//     case >=3:
+//     Console.WriteLine("cool");
+//     break;
+//     default:
+//     Console.WriteLine("ugh");
+//     break;
+// }
+
+
