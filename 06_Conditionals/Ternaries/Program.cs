d1feb92bb391a93382c533c03ba5fce1dd166e4b
cstazinski29 <cstazinski@gmail.com>
2022-02-03 10:32:06 -0500

    adding ternaries to fundamental solution/project

---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter age:");
+string response = System.Console.ReadLine();
+int age = int.Parse(response);
+
+string output = age >= 18 ? "You can vote!!" : "You can't vote";
+
+Console.WriteLine(output);
